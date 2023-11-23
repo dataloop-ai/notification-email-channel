e85d992595eb68580d77e01f9b91ed72ab4a5ca9
shlomi <shlomi.s@dataloop.ai>
2023-11-23 13:48:48 +0200

    align template resolver

--- modules/templateResolver.py
+++ modules/templateResolver.py
@@ -3,10 +3,18 @@ from modules.notificationInfo import EmailTemplate
 
 class TemplateResolver:
     @staticmethod
+    def get_project_invitation_code():
+        return 'Platform.Invitations.ProjectInvitation'
+
+    @staticmethod
+    def get_organization_invitation_code():
+        return 'Platform.Invitations.OrganizationInvitation'
+
+    @staticmethod
     def resolve_template(notification_code):
-        if notification_code == 'Platform.Invitations.ProjectInvitation':
+        if notification_code == TemplateResolver.get_project_invitation_code():
             return EmailTemplate.PROJECT_INVITE
-        elif notification_code == 'Platform.Invitations.OrganizationInvitation':
+        elif notification_code == TemplateResolver.get_organization_invitation_code():
             return EmailTemplate.ORG_INVITE
         else:
             return EmailTemplate.NOTIFICATION
